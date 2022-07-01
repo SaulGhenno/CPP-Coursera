3e8aa2f10e23a6933071b11abfe7eb9be754104f
Saul Ghenno <saul.ghenno@gmail.com>
2022-07-01 17:47:53 -0500

    Adding comments

--- task1.cpp
+++ task1.cpp
@@ -5,17 +5,20 @@
 //   change array to vector<>
 //   inline any short function
 
-
+// #include <stdio.h>
 #include <iostream>
 #include <vector>
 using namespace std;
+// #define N 40
 const int N = 40;
 
+//assignation function definition
 inline void assign(int n, vector<int>& data)
 {
     for (int i = 0; i < n; i++)     data[i] = i;
 }
 
+//sum function definition
 inline void sum(int& p, int n, vector<int>& data)
 {
     for (int i = 0; i < n; ++i)     p = p + data[i];
@@ -23,9 +26,15 @@ inline void sum(int& p, int n, vector<int>& data)
 
 int main(void)
 {
+    // code instructions of c
+    // int i;
+    // int accum = 0;
     int acum = 0;
+    // int data[N];
     vector<int> data(N);
     assign(N, data);
+    // sum(&accum, N, data);
     sum(acum, N, data);
+    // printf("sum is %d\n", accum);
     cout << "sum is " << acum << endl;
 }
