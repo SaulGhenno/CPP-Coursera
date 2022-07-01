26e3b755f2dfc5feaf7d11d2464089daafe4c220
Saul Ghenno <saul.ghenno@gmail.com>
2022-07-01 17:38:13 -0500

    Final change of task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,25 +1,31 @@
-#include <iostream>
+//   Convert this program to C++
+//   change to C++ io
+//   change to one line comments
+//   change defines of constants to const
+//   change array to vector<>
+//   inline any short function
 
+
+#include <iostream>
+#include <vector>
 using namespace std;
 const int N = 40;
 
-inline int sum(int& p, int n, int d[])
+inline void assign(int n, vector<int>& data)
 {
-    for (int i = 0; i < n; ++i)
-    {
-        p = p + d[i];
-    }
+    for (int i = 0; i < n; i++)     data[i] = i;
+}
+
+inline void sum(int& p, int n, vector<int>& data)
+{
+    for (int i = 0; i < n; ++i)     p = p + data[i];
 }
 
 int main(void)
 {
-    int acum;
-    //int data[N];
+    int acum = 0;
     vector<int> data(N);
-    for (int i = 0; i < N; ++i)
-    {
-        data[i] = i;
-    }
-    sum(acum, N, d);
+    assign(N, data);
+    sum(acum, N, data);
     cout << "sum is " << acum << endl;
 }
