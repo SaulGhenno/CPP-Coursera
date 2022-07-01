2bc015a0695dd033df0cc8586aa5914b96f162de
Saul Ghenno <saul.ghenno@gmail.com>
2022-07-01 15:57:08 -0500

    Some more changes to task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 const int N = 40;
+
 inline int sum(int& p, int n, int d[])
 {
     for (int i = 0; i < n; ++i)
@@ -14,11 +15,11 @@ int main(void)
 {
     int acum;
     //int data[N];
-    vector<int> data;
+    vector<int> data(N);
     for (int i = 0; i < N; ++i)
     {
         data[i] = i;
-        sum(acum, N, data);
-        cout << "sum is " << acum << endl;
     }
+    sum(acum, N, d);
+    cout << "sum is " << acum << endl;
 }
