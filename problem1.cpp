de4a6624872a62172adce34dd39408e2a93e0364
Saul Ghenno <saul.ghenno@gmail.com>
2022-07-01 12:44:22 -0500

    Adding the main function to problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -11,3 +11,13 @@ using namespace std;
 const int sides = 6; //replaces many sharp defines
 inline int r_sides(){return (rand() % sides + 1);}
 
+int main(void)
+{
+    const int n_dice = 2;
+    int d1, d2;
+    srand(clock());
+    cout << "\nEnter number of trials: ";
+    int trials;
+    cin >> trials; // compare to scanf
+    int *outcomes = new int[n_dice * sides + 1];
+}
