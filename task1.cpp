4faafb8339e0dfcef485f40f0ac7c516ac0da3e1
Saul Ghenno <saul.ghenno@gmail.com>
2022-07-01 15:42:17 -0500

    Test of changes in task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,17 +1,24 @@
-#include <iostream.h>
+#include <iostream>
 
 using namespace std;
 const int N = 40;
-inline int sum(int *p, int n, int d[]){for (int i = 0; i < n; ++i){*p = *p + d[i];}}
+inline int sum(int& p, int n, int d[])
+{
+    for (int i = 0; i < n; ++i)
+    {
+        p = p + d[i];
+    }
+}
 
 int main(void)
 {
     int acum;
-    int data[N];
+    //int data[N];
+    vector<int> data;
     for (int i = 0; i < N; ++i)
     {
         data[i] = i;
-        sum(&acum, N, data);
+        sum(acum, N, data);
         cout << "sum is " << acum << endl;
     }
 }
